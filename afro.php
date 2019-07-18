388ec299751c78974d6ab624722663a5800c9624
leratolethabo <50952251+leratolethabo@users.noreply.github.com>
2019-07-18 23:35:32 +0200

    Add files via upload

---
+++ afro.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+
+</head>
+<body>
+	<h1>life is like a box of chocolate</h1>
+	<p>you will not you might, you will have turbulents</p>
+	<p>why not you, who will you suggest</p>
+	<?php include 'life.php'; ?>
+
+</body>
+</html>
